6978abfe6aa156a68bdb02217a95945c7c29c1ca
mariddsouza <marianadesouza.cnt@gmail.com>
2023-05-22 16:08:58 -0300

    Add: Update fmt

--- backend.tf
+++ backend.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
 
 resource "aws_instance" "server_wordpress" {
-  ami           = "ami-0c94855ba95c71c99" # substitua pela AMI desejada
-  instance_type = "t2.micro"             
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Server Instance"
   }
 
-  key_name = "your-key-pair" # substitua pelo nome do par de chaves existente na sua conta AWS
+  key_name = "key-05abb699be"
 
   user_data = <<-EOF
     #!/bin/bash
@@ -19,7 +19,7 @@ resource "aws_instance" "server_wordpress" {
     systemctl start httpd
     systemctl enable httpd
 
-    # Instalação do WordPress
+    # inicialização WordPress
     wget https://wordpress.org/latest.tar.gz
     tar -xzf latest.tar.gz -C /var/www/html/
     chown -R apache:apache /var/www/html/wordpress
@@ -31,10 +31,7 @@ resource "aws_instance" "server_wordpress" {
     systemctl restart httpd
   EOF
 
-  # Regras de segurança
-  vpc_security_group_ids = ["your-security-group"]
-
-  # Definição de regras de ingresso
+  # Porta 80 exposta
   ingress {
     from_port   = 80
     to_port     = 80
@@ -42,40 +39,34 @@ resource "aws_instance" "server_wordpress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Porta meu IP
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["127.0.0.1"] 
+    cidr_blocks = ["127.0.0.1/32"]
   }
 
-  # Definição de regras de egresso (saída)
-  #egress {
-    #from_port   = 0
-    #to_port     = 0
-    #protocol    = "-1"
-    #cidr_blocks = ["0.0.0.0/0"]
-  #}
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
+output "ip_publico" {
+  value = aws_instance.server_wordpress.ip_publico
 }
 
-resource "aws_autoscaling_group" "example" {
-  name                 = "example-asg"
+resource "aws_autoscaling_group" "escalonamento-server" {
+  name                 = "escalonamento-server"
   min_size             = 1
   max_size             = 10
   desired_capacity     = 1
-  launch_configuration = aws_launch_configuration.example.name
+  launch_configuration = aws_launch_configuration.escalonamento-server.name
 
   tag {
-    key                 = "Name"
+    key                 = "Escalonamento-server-key"
     value               = "example-instance"
     propagate_at_launch = true
   }
 
-  #Especifico para o monitoramento da CPU
+  #Especifico para o monitoramento da CPU 90%
   metric {
     name               = "CPUUtilization"
     namespace          = "AWS/EC2"
@@ -84,33 +75,33 @@ resource "aws_autoscaling_group" "example" {
     threshold          = 90
     evaluation_periods = 1
     period             = 60
-    alarm_actions      = [aws_autoscaling_policy.example.arn]
+    alarm_actions      = [aws_autoscaling_policy.escalonamento-server.arn]
   }
 }
 
-resource "aws_autoscaling_policy" "example" {
-  name                   = "example-asg-policy"
+resource "aws_autoscaling_policy" "escalonamento-policy" {
+  name                   = "escalonamento-policy"
   policy_type            = "SimpleScaling"
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = 1
   cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.example.name
+  autoscaling_group_name = aws_autoscaling_group.escalonamento-policy.name
 }
 
-resource "aws_cloudwatch_metric_alarm" "example" {
-  alarm_name          = "example-asg-alarm"
+resource "aws_cloudwatch_metric_alarm" "alarme" {
+  alarm_name          = "alarme-server"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 90
   period              = 60
-  alarm_description   = "This metric monitors CPU utilization"
+  alarm_description   = "Alarme para monitorar CPU"
   namespace           = "AWS/EC2"
   metric_name         = "CPUUtilization"
   statistic           = "Average"
   unit                = "Percent"
-  alarm_actions       = [aws_autoscaling_policy.example.arn]
+  alarm_actions       = [aws_autoscaling_policy.alarme.arn]
   dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.example.name
+    AutoScalingGroupName = aws_autoscaling_group.alarme.name
   }
 }
 
